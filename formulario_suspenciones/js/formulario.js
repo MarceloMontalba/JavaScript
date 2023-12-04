559c80d2cdb1ab1bfbd2999e6954e740d2ab201a
Marcelo Montalba Pavez <mmontalbapavez@gmail.com>
2023-12-03 23:00:19 -0300

    Formulario Suspensión de Beneficios UCT
    Bosquejo formulario suspensión de beneficios UCT.

--- formulario_suspenciones/js/formulario.js
+++ formulario_suspenciones/js/formulario.js
@@ -1,8 +1,12 @@
 let formulario = document.querySelector(".formulario-solicitud");
+let progreso = document.querySelectorAll(".barra-progreso-opcion");
+
 formulario.addEventListener("click", function(e){
   let elemento = e.target;
   let esBotonSiguiente = elemento.classList.contains("paso-boton-siguiente");
   let esBotonRegreso   = elemento.classList.contains("paso-boton-regresar");
+
+  let esPaso = elemento.classList.contains("paso-pasar");
 
   if(esBotonSiguiente || esBotonRegreso) {
     let pasoActual = document.getElementById("paso-"+elemento.dataset.step);
@@ -13,9 +17,15 @@ formulario.addEventListener("click", function(e){
     
     if(esBotonSiguiente) {
       pasoSaltar.classList.add("paso-izquierda");
+      if(esPaso){
+        progreso[elemento.dataset.paso_actual-1].classList.add('barra-progreso-opcion-activo');
+      }
     } 
     if(esBotonRegreso){
       pasoActual.classList.remove("paso-izquierda");
+      if(esPaso){
+        progreso[elemento.dataset.paso_actual-1].classList.remove('barra-progreso-opcion-activo'); 
+      }
     }
   }
 })
