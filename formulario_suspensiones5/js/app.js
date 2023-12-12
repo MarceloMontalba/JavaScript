15c6cd7ba5d789189bc6b9ce5dd6ab6776a42044
mmontalba <mmontalbapavez@gmail.com>
2023-12-12 03:07:16 -0300

    Maqueta Formulario Solicitudes Estudiantes UCT

--- formulario_suspensiones5/js/app.js
+++ formulario_suspensiones5/js/app.js
@@ -28,6 +28,32 @@ $(document).ready(function(){
   // Selecciones Estarán Ocultas
   $(".seleccion-tipo-solicitud").hide();
 
+  // Secciones de los formularios que estan invosibles al inicio
+  $("#suspension-beneficios1").hide();
+  $("#suspension-confirmacion1").hide();
+
   // Se visualiza la solicitud deseada
   $(".btn-solicitud").click(function(){seleccionaTipoSolicitud($(this))});
+
+  // Botones continuar 
+  $(".boton-continuar").click(function(){
+    let idBoton = $(this).attr("id");
+
+    if(idBoton === "btn-continuar-suspension") {
+      $("#suspension-academica").hide();
+      $("#suspension-beneficios1").show();
+    }
+    if(idBoton === "btn-regresar-suspension-academica") {
+      $("#suspension-beneficios1").hide();
+      $("#suspension-academica").show();
+    }
+    if(idBoton === "btn-continuar-confirmacion1") {
+      $("#suspension-beneficios1").hide();
+      $("#suspension-confirmacion1").show();
+    }
+    if(idBoton == "btn-regresar-suspension-beneficios1") {
+      $("#suspension-confirmacion1").hide();
+      $("#suspension-beneficios1").show();
+    }
+  });
 })
