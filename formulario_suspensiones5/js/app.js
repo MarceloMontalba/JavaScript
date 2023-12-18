746c38dfb9f8a48a22c978642b77836a0011ad41
Marcelo Montalba Pavez <mmontalbapavez@gmail.com>
2023-12-18 11:54:18 -0300

    Actualizacion Maqueta Solicitud de Estudiantes UCT

--- formulario_suspensiones5/js/app.js
+++ formulario_suspensiones5/js/app.js
@@ -35,54 +35,55 @@ function seleccionaTipoSolicitud(boton){
 
 // Funcion para cambiar de vista de cada uno de los pasos
 function cambiarPaso(boton){
+  let listaIds = "#seleccion-suspensiones, #seleccion-renuncia";
+  let tipoFormulario = boton.closest(listaIds).attr("id");
   let idBoton = $(boton).attr("id");
-    let pasosProgreso = $(".barra-progreso-opcion");
-    let pasoActual = $(boton).attr("data-step");
+  let pasosProgreso = $("#"+tipoFormulario+" .barra-progreso-opcion");
+  let pasoActual = $(boton).attr("data-step");
 
-    // Se cambian los check de los pasos segun sea el caso
-    if(boton.hasClass("boton-continua")){
-      pasosProgreso[pasoActual].classList.add("barra-progreso-opcion-activo");
-      if(pasoActual-1>=0){
-        pasosProgreso[pasoActual-1].classList.remove("barra-progreso-opcion-activo");
-        pasosProgreso[pasoActual-1].classList.add("barra-progreso-opcion-completado");
-      }
-    }else {
-      if(boton.hasClass("boton-regresa")){
-        pasosProgreso[pasoActual-1].classList.remove("barra-progreso-opcion-activo");
-        console.log(pasoActual)
-        if(pasoActual-2>=0){
-          pasosProgreso[pasoActual-2].classList.remove("barra-progreso-opcion-completado");
-          pasosProgreso[pasoActual-2].classList.add("barra-progreso-opcion-activo");
-        }
+  // Se cambian los check de los pasos segun sea el caso
+  if(boton.hasClass("boton-continua")){
+    pasosProgreso[pasoActual].classList.add("barra-progreso-opcion-activo");
+    if(pasoActual-1>=0){
+      pasosProgreso[pasoActual-1].classList.remove("barra-progreso-opcion-activo");
+      pasosProgreso[pasoActual-1].classList.add("barra-progreso-opcion-completado");
+    }
+  }else {
+    if(boton.hasClass("boton-regresa")){
+      pasosProgreso[pasoActual-1].classList.remove("barra-progreso-opcion-activo");
+      if(pasoActual-2>=0){
+        pasosProgreso[pasoActual-2].classList.remove("barra-progreso-opcion-completado");
+        pasosProgreso[pasoActual-2].classList.add("barra-progreso-opcion-activo");
       }
     }
+  }
 
-    // Se ocultan todos los pasos excepto el actual
-    $(".paso-suspensiones").hide();
-    if(idBoton === "btn-continuar-suspension") {
-      $("#aviso-suspension-beneficios1").show();
-    }
-    if(idBoton === "btn-continuar-suspension2") {
-      $("#suspension-beneficios1").show();
-    }
-    if(idBoton === "btn-continuar-confirmacion1") {
-      $("#suspension-confirmacion1").show();
-    }
-    if(idBoton === "btn-regresar-suspension-beneficios1") {
-      $("#suspension-beneficios1").show();
-    }
-    if(idBoton === "btn-aviso-suspension-beneficios1") {
-      $("#aviso-suspension-beneficios1").show();
-    }
-    if(idBoton === "btn-regresar-suspension-academica") {
-      $("#suspension-academica").show();
-    }
-    // Se redirecciona a la cabecera del modulo
-    let cabeceraApartado = $("#posicionador-scroll").offset().top-110;
-    console.log(cabeceraApartado)
-    $(".contenedor-principal").animate({
-      scrollTop: "+=" + cabeceraApartado
-    }, 900)
+  // Se ocultan todos los pasos de la suspension excepto el actual
+  $("#"+tipoFormulario+" .paso-suspensiones").hide();
+  if(idBoton === "btn-continuar-suspension") {
+    $("#"+tipoFormulario+" #aviso-suspension-beneficios1").show();
+  }
+  if(idBoton === "btn-continuar-suspension2") {
+    $("#"+tipoFormulario+" #suspension-beneficios1").show();
+  }
+  if(idBoton === "btn-continuar-confirmacion1") {
+    $("#"+tipoFormulario+" #suspension-confirmacion1").show();
+  }
+  if(idBoton === "btn-regresar-suspension-beneficios1") {
+    $("#"+tipoFormulario+" #suspension-beneficios1").show();
+  }
+  if(idBoton === "btn-aviso-suspension-beneficios1") {
+    $("#"+tipoFormulario+" #aviso-suspension-beneficios1").show();
+  }
+  if(idBoton === "btn-regresar-suspension-academica") {
+    $("#"+tipoFormulario+" #suspension-academica").show();
+  }
+
+  // Se redirecciona a la cabecera del modulo
+  let cabeceraApartado = $("#posicionador-scroll").offset().top-110;
+  $(".contenedor-principal").animate({
+    scrollTop: "+=" + cabeceraApartado
+  }, 900)
 }
 
 $(document).ready(function(){
@@ -90,9 +91,13 @@ $(document).ready(function(){
   $(".seleccion-tipo-solicitud").hide();
 
   // Secciones de los formularios que estan invosibles al inicio
-  $("#aviso-suspension-beneficios1").hide();
-  $("#suspension-beneficios1").hide();
-  $("#suspension-confirmacion1").hide();
+  $("#seleccion-suspensiones #aviso-suspension-beneficios1").hide();
+  $("#seleccion-suspensiones #suspension-beneficios1").hide();
+  $("#seleccion-suspensiones #suspension-confirmacion1").hide();
+
+  $("#seleccion-renuncia #aviso-suspension-beneficios1").hide();
+  $("#seleccion-renuncia #suspension-beneficios1").hide();
+  $("#seleccion-renuncia #suspension-confirmacion1").hide();
 
   // Se visualiza la solicitud deseada
   $(".btn-solicitud").click(function(){seleccionaTipoSolicitud($(this))});
